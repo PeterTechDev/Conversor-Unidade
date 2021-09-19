302d18647d3c1b796245913b263fcf8e5e233fe8
Peter Leite <peterleite.dev@gmail.com>
2021-09-19 20:13:20 -0300

    arquivos do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+const libra = 0.45359237
+
+let entry = document.querySelector("#kg")
+const submitBtn = document.querySelector("button")
+const result = document.getElementById('result')
+
+submitBtn.addEventListener('click', () => {
+    if (entry.value == '') {
+        alert('Favor informar algum valor')
+    }
+    result.innerText = (entry.value * libra).toFixed(2) + "kg";
+    entry.value = ''
+})
