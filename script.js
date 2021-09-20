a268aefba02b9c22690869788b7371afb03d0b8f
Peter Leite <peterleite.dev@gmail.com>
2021-09-20 08:18:25 -0300

    added animação

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ submitBtn.addEventListener('click', () => {
         alert('Favor informar algum valor')
         prevantDefault()
     }
+    result.classList.remove('rotate')
+    void result.offsetWidth;
+    result.classList.add('rotate')
     result.innerText = (entry.value * libra).toFixed(2) + "kg";
     entry.value = ''
 })
+
+// Descobri como restartar a animação usando void no css tricks
+// https://css-tricks.com/restart-css-animation/
